1a26565913b284f529a81a74b8ab008793871ed1
sakthiram2004 <your.email@example.com>
2024-01-16 08:27:11 +0530

    solution

---
+++ LUDO.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc =new Scanner (System.in);
+		int x=sc.nextInt();
+		for(int i=0;i<x;i++)
+		{
+		    System.out.println((sc.nextInt()>=6)?("YES"):("NO"));
+		}
+
+	}
+}
